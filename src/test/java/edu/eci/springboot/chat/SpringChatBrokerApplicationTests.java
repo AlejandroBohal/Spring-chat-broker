1b2fa8adc55bd9a6a9550c0cce2e522cc5108687
Alejandro Bohorquez <sergio.bohorquez@mail.escuelaing.edu.co>
2020-07-06 15:22:53 -0500

    First Commit
    initializing project

---
+++ src/test/java/edu/eci/springboot/chat/SpringChatBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.springboot.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
